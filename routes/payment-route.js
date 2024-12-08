c7f13839efb1b94449545333e27b3c05a01975ee
KietCSE <ptkiet170104@gmail.com>
2024-12-08 20:02:09 +0700

    feat: add validation rule

--- routes/payment-route.js
+++ routes/payment-route.js
@@ -1,13 +1,14 @@
 import { Router } from "express"
 import paymentController from "../controllers/payment-controller.js"
+import { validatePaymentRule, validationRequest, validateCheckStatusRule } from '../middlewares/payment.js'
 
 const router = Router()
 
-router.post('/momo', paymentController.createPaymentLink)
+router.post('/momo', validatePaymentRule, validationRequest, paymentController.createPaymentLink)
 router.post('/result', paymentController.handleDataFromMomoService)
-router.get('/status/:id', paymentController.checkStatusPayment)
-router.patch('/balance', paymentController.updateBalance)
+router.get('/status/:id', validateCheckStatusRule, validationRequest, paymentController.checkStatusPayment)
 router.get('/combo', paymentController.loadComboData)
+// router.patch('/balance', paymentController.updateBalance)
 
 export default router
 
--- routes/systemconfig-route.js
+++ routes/systemconfig-route.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import systemconfigController from "../controllers/systemconfig-controller.js"
+import { validateConfigUpdateRule, validationRequest, validateLoadConfigRule } from "../middlewares/systemconfig.js"
 
 const router = Router()
 
-router.get('/load-file/:filename', systemconfigController.loadFileData)
-router.put('/update', systemconfigController.updateFileData)
+router.get('/load-file/:filename', validateLoadConfigRule, validationRequest, systemconfigController.loadFileData)
+router.put('/update', validateConfigUpdateRule, validationRequest, systemconfigController.updateFileData)
 
 export default router
 
