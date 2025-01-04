cf1607b36101c20d2b31338a3b27d9fb5c06bc40
RavenTheshadow <hogiathang2@gmail.com>
2025-01-04 11:11:28 +0700

    Add cors

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ connectRedis()
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://ebc7-171-247-146-191.ngrok-free.app'],
+    origin: ['http://localhost:5173', 'https://ebc7-171-247-146-191.ngrok-free.app', 'http://ec2-175-41-171-194.ap-southeast-1.compute.amazonaws.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
